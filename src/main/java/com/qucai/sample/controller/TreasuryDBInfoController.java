b5bb515d430713b79003828973b07709f8cd99f2
yaospear <yaospear@163.com>
2021-03-17 17:37:18 +0800

    message

--- src/main/java/com/qucai/sample/controller/TreasuryDBInfoController.java
+++ src/main/java/com/qucai/sample/controller/TreasuryDBInfoController.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang3.StringUtils;
-import org.apache.struts2.views.xslt.ArrayAdapter;
+//import org.apache.struts2.views.xslt.ArrayAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
