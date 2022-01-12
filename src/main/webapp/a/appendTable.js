84f9c83b437b73328f597252b97c4664e823e0d6
yaospear <yaospear@163.com>
2022-01-12 10:36:10 +0800

    debug personal statistic layout

--- src/main/webapp/a/appendTable.js
+++ src/main/webapp/a/appendTable.js
@@ -6,7 +6,7 @@ function appendTable(sb) {
     <td name="ClearPaid" id="ClearPaid" value="${item.t_Txn_Static_Clear_his}" >${item.t_Txn_Static_Clear_his}</td>
     <td class="col-xs-push-1" style="word-wrap:break-word;word-break:break-all;mso-number-format:'\\@';"><div id="printID" value="${item.t_Txn_Static_PerName}">${item.t_Txn_Static_PerName}</div> </td>
     <td class="col-xs-push-1" style="word-wrap:break-word;word-break:break-all;">${item.t_Txn_Static_PerID}</td>
-    <td class="col-xs-push-1" style="word-wrap:break-word;word-break:break-all;">{item.t_Txn_Static_PerCreditCard}</td>
+    <td class="col-xs-push-1" style="word-wrap:break-word;word-break:break-all;">${item.t_Txn_Static_PerCreditCard}</td>
     <td class="col-xs-push-1" style="word-wrap:break-word;word-break:break-all;">&yen${item.t_Txn_Static_TotTxnAmt}</td>
     <td class="col-xs-push-1" style="word-wrap:break-word;word-break:break-all;">&yen${item.t_Txn_Static_TotTxnActAmt}</td>
     <td class="col-xs-push-1" style="word-wrap:break-word;word-break:break-all;">${item.t_Txn_Static_TotInterest}</td>
